ac8dbb4d62aa2a5a1ea48b176cbf23fdafdebdab
Law93D <lawtaden@gmail.com>
2024-04-04 09:03:27 -0700

    web

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,29 +1,30 @@
 #!/usr/bin/python3
-"""Fabric script that distributes an archive to your web servers"""
+"""
+Fabric script based on the file 1-pack_web_static.py that distributes an
+archive to the web servers
+"""
 
-from fabric.api import env, put, run
+from fabric.api import put, run, env
 from os.path import exists
 env.hosts = ['34.227.93.156', '3.85.136.88']
 
 
 def do_deploy(archive_path):
-    """Distributes an archive to your web servers"""
-    if not exists(archive_path):
+    """distributes an archive to the web servers"""
+    if exists(archive_path) is False:
         return False
     try:
-        put(archive_path, "/tmp/")
-        filename = archive_path.split('/')[-1]
-        foldername = filename.split('.')[0]
-        run("mkdir -p /data/web_static/releases/{}/".format(foldername))
-        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
-            .format(filename, foldername))
-        run("rm /tmp/{}".format(filename))
-        run("mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/"
-            .format(foldername, foldername))
-        run("rm -rf /data/web_static/releases/{}/web_static".format(foldername))
-        run("rm -rf /data/web_static/current")
-        run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
-            .format(foldername))
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
+        path = "/data/web_static/releases/"
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
         return True
     except:
         return False
